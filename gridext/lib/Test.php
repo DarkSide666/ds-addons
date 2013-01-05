6f218d83e694f845ab9229e432d36af170492715
Imants Horsts <imants.horsts@inbox.lv>
2013-01-05 13:00:09 +0200

    GridExt: update test page

--- gridext/lib/Test.php
+++ gridext/lib/Test.php
@@ -5,15 +5,23 @@ class Test extends \View{
 	function init(){
 		parent::init();
         $this->api->dbConnect();
-		
+        
+        $t = $this->add('Tabs');
+   		$t->addTab('Toolbars')->add(__NAMESPACE__ . '/TestToolbars');
+   		$t->addTab('Extended Search')->add(__NAMESPACE__ . '/TestExtendedSearch');
+    }
+}
+
+
+
+class TestExtendedSearch extends \View{
+	function init(){
+		parent::init();
+
         $cols = $this->add('Columns');
         $c1 = $cols->addColumn(6);
         $c2 = $cols->addColumn(6);
 
-        // --- ExtendedSearch -------------------------------------------------
-        $c1->add('H2')->set('ExtendedSearch');
-        // --------------------------------------------------------------------
-        
 		// Grid
 		$c1->add('H4')->set('Grid_Extended');
 		$g = $c1->add(__NAMESPACE__ . '/Grid_Extended');
@@ -23,22 +31,33 @@ class Test extends \View{
 		$g->addExtendedSearch(array('id','name','parent_id'));
 
 		// CRUD
-		$c1->add('H4')->set('CRUD_Extended');
-		$g = $c1->add(__NAMESPACE__ . '/CRUD_Extended');
+		$c2->add('H4')->set('CRUD_Extended');
+		$g = $c2->add(__NAMESPACE__ . '/CRUD_Extended');
 		$g->setModel('tests/Test');
 		if($g->grid) {
 			$g->grid->addPaginator(4);
 			$g->grid->addQuickSearch(array('name'));
 			$g->grid->addExtendedSearch(array('id','name','parent_id'));
 		}
+	}
+}
+
+
+
+class TestToolbars extends \View{
+	function init(){
+		parent::init();
+
+        $cols = $this->add('Columns');
+        $c1 = $cols->addColumn(6);
+        $c2 = $cols->addColumn(6);
 
 		// --- Toolbar --------------------------------------------------------
-        $c2->add('H2')->set('Toolbar');
+        $c1->add('H2')->set('One Toolbar');
 		// --------------------------------------------------------------------
 
 		// Grid
-		$c2->add('H4')->set('Grid_Extended');
-		$g = $c2->add(__NAMESPACE__ . '/Grid_Extended');
+		$g = $c1->add(__NAMESPACE__ . '/Grid_Extended');
 		$g->setModel('tests/Test');
 		$g->addPaginator(4);
 		$g->addToolbar()
@@ -46,12 +65,11 @@ class Test extends \View{
             ->setHTML('<b>This is simple toolbar. You can add anything here or extend this with additional functionality!</b>');
 
 		// --- Multiple Toolbars ----------------------------------------------
-        $c2->add('H2')->set('Multiple Toolbars');
+        $c1->add('H2')->set('Multiple Toolbars');
 		// --------------------------------------------------------------------
 		
 		// Grid
-		$c2->add('H4')->set('Grid_Extended');
-		$g = $c2->add(__NAMESPACE__ . '/Grid_Extended');
+		$g = $c1->add(__NAMESPACE__ . '/Grid_Extended');
 		$g->setModel('tests/Test');
 		$g->addPaginator(4);
 		$g->addToolbar()
@@ -64,25 +82,14 @@ class Test extends \View{
             ->add('Text')
             ->setHTML('<b>Last but not least toolbar.</b>');
 
-		// --- ActionToolbar --------------------------------------------------
-        $c2->add('H2')->set('ActionToolbar');
+		// --- Action Toolbar -------------------------------------------------
+        $c2->add('H2')->set('Action Toolbar');
 		// --------------------------------------------------------------------
 		
 		// Grid
-		$c2->add('H4')->set('Grid_Extended');
 		$g = $c2->add(__NAMESPACE__ . '/Grid_Extended');
 		$g->setModel('tests/Test');
 		$g->addPaginator(4);
 		$g->addActionToolbar();
-
-		// CRUD
-		$c2->add('H4')->set('CRUD_Extended');
-		$crud = $c2->add(__NAMESPACE__ . '/CRUD_Extended');
-		$crud->setModel('tests/Test');
-		if($g=$crud->grid) {
-			$g->addPaginator(4);
-			$g->addActionToolbar();
-		}
-
 	}
 }
